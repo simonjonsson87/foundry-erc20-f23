33e1ee3a59676d202abc2c2f781aba78f34e42bb
Simon Jonsson <simonjonsson87@gmail.com>
2025-01-20 23:11:00 +0000

    Added deploy script

--- script/DeploySimonCoin.s.sol
+++ script/DeploySimonCoin.s.sol
@@ -5,5 +5,12 @@ import {Script} from "forge-std/Script.sol";
 import {SimonCoin} from "src/SimonCoin.sol";
 
 contract DeploySimonCoin is Script {
+    uint256 private constant INITIAL_SUPPLY = 1_000_000 ether;
 
+    function run () public  returns (SimonCoin) {
+        vm.startBroadcast();
+        SimonCoin coin = new SimonCoin(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+        return coin;
+    }
 }
